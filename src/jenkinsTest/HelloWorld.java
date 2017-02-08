9ba5f22c78d3cc62d9d98e00a25fe98ffd558a82
Rahul Singh <mail.rahul112@yahoo.in>
2017-02-08 09:24:46 +0530

    Added factorial program

--- src/jenkinsTest/HelloWorld.java
+++ src/jenkinsTest/HelloWorld.java
@@ -1,11 +1,27 @@
 package jenkinsTest;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-
+		int n, c, fact = 1;
+		 
+	      System.out.println("Enter an integer to calculate it's factorial");
+	      Scanner in = new Scanner(System.in);
+	 
+	      n = in.nextInt();
+	 
+	      if ( n < 0 )
+	         System.out.println("Number should be non-negative.");
+	      else
+	      {
+	         for ( c = 1 ; c <= n ; c++ )
+	            fact = fact*c;
+	 
+	         System.out.println("Factorial of "+n+" is = "+fact);
+	      }
+	   }
 	}
-
-}
